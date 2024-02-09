c89246a4d6232400bc1fbcb8f3bae432ac3b8e78
David Casanellas <davidcasanell@gmail.com>
2024-02-09 19:18:09 +0100

    wip

--- src/api/gameFunctions.js
+++ src/api/gameFunctions.js
@@ -1,24 +1,33 @@
-import fire from '../fire'
-import Cookies from 'universal-cookie';
+import fire from "../fire";
+import Cookies from "universal-cookie";
 
 let cookies = new Cookies();
 
 export function changeUserName(name) {
-  let ref = fire.database().ref().child('Players')
-  let key = cookies.get('key')
-  let updates = {}
+  let ref = fire.database().ref().child("Players");
+  let key = cookies.get("key");
+  let updates = {};
   updates[key] = {
     username: name,
-  }
-  ref.update(updates)
+  };
+  ref.update(updates);
 }
 
 export function setUserCharacterType(type) {
-  let ref = fire.database().ref().child('Players')
-  let key = cookies.get('key')
-  let updates = {}
+  let ref = fire.database().ref().child("Players");
+  let key = cookies.get("key");
+  let updates = {};
   updates[key] = {
     characterType: type,
-  }
-  ref.update(updates)
+  };
+  ref.update(updates);
+}
+
+export function getPlayer() {
+  const player = {};
+  let ref = fire.database().ref().child("Players");
+  console.log(ref);
+  let key = cookies.get("key");
+  console.log(key);
+  return player;
 }
--- src/components/Name.js
+++ src/components/Name.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { useTranslation } from "react-i18next"
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 
-export default function Home() {
-    const [t, i18n] = useTranslation("global")
-    
-    return (
-        <React.Fragment>
-            <h1>*NAME*</h1>
-        </React.Fragment>
-    );
+import { getPlayer } from "../api/gameFunctions";
+
+export default function Name() {
+  const [t, i18n] = useTranslation("global");
+  const players = getPlayer();
+  console.log("players", players);
+  //   if (isLogged) {
+  //     console.log("key", key);
+  // let ref = fire.database().ref().child("Players");
+  // console.log("ref", ref);
+  // ref.once("value", function (playersStateSnap) {
+  //   let players = playersStateSnap.val();
+  // });
+  //   }
+
+  return (
+    <React.Fragment>
+      <h1>*NAME*</h1>
+    </React.Fragment>
+  );
 }
